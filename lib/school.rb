3fd449102859c082862154edf51455465a002a20
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-30 00:30:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,12 @@
 class School
-  attr_reader :name
+  attr_reader :name, :roster
   
   def initialize(name)
     name = @name
     @roster = {}
   end
   
-  def roster
-    @roster
-  end
+  
   
   def add_student(name, grade)
     @roster.push(name, grade)
